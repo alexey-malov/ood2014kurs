d016a7d2534a538ee69f989a55830894becd9d06
Alexey Malov <alexey.malov@cpslabs.net>
2014-04-18 20:31:38 +0400

    Добавлены тесты для libqp

---
+++ QuizPlayer/libqp/libqptests/QuizPlayerTests.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "QuizPlayer.h"
+
+
+BOOST_AUTO_TEST_SUITE(QuizPlayerTests)
+
+using namespace qp;
+
+
+
+BOOST_AUTO_TEST_CASE(Constructor)
+{
+	CQuizPlayer qp;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
