053aba3bd507ad04043b5fd33f905c44a1a7d31c
alisa-teplouhova <flovettee@gmail.com>
2014-04-19 06:01:35 +0400

    Добавлена функция для проверки неотрицательности аргумента

--- QuizPlayer/libqp/utils.h
+++ QuizPlayer/libqp/utils.h
@@ -11,4 +11,14 @@ inline T const& VerifyIfNotEmpty(T const& collection, std::string const& errorMe
 	}
 	return collection;
 }
+
+template <typename T>
+inline T const& VerifyIfNotNegative(T const& number, std::string const& errorMessage = std::string("Negative number is not allowed"))
+{
+	if (number < 0)
+	{
+		throw std::invalid_argument(errorMessage);
+	}
+	return number;
+}
 }
