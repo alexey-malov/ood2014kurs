5e40f97120ab677da73bea1b8003452edcc11078
Alexey Malov <alexey.malov@cpslabs.net>
2014-04-24 20:31:35 +0400

    выпилен лишний код из тестов

--- QuizPlayer/libqp/libqptests/MultipleChoiceQuestionTests.cpp
+++ QuizPlayer/libqp/libqptests/MultipleChoiceQuestionTests.cpp
@@ -10,10 +10,6 @@ BOOST_AUTO_TEST_CASE(Construction)
 	CMultipleChoiceQuestion quest1("Question description");
 	BOOST_REQUIRE_EQUAL(quest1.GetDescription(), "Question description");
 	BOOST_REQUIRE_CLOSE(quest1.GetScore(), 0.0, 0.0001);
-	BOOST_REQUIRE(!quest1.GetCorrectFeedback());
-	BOOST_REQUIRE(!quest1.GetIncorrectFeedback());
-
-	BOOST_REQUIRE_THROW(CMultipleChoiceQuestion quest2(""), std::invalid_argument);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
