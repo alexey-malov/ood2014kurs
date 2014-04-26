4d13d82c94627cc55f3c303fb4b451b984805cc0
Vladimir Polubarjev <vp2home@gmail.com>
2014-04-26 23:51:39 +0400

    MultipleChoiceQuestionTests.QuestionHasChoices упростить - достаточно проверки, что класс multiplechoicequestion унаследован от questionwithchoices, т.е. соотв. Dynamic-cast не бросает исключения

--- QuizPlayer/libqp/libqptests/MultipleChoiceQuestionTests.cpp
+++ QuizPlayer/libqp/libqptests/MultipleChoiceQuestionTests.cpp
@@ -16,34 +16,8 @@ BOOST_AUTO_TEST_CASE(Construction)
 BOOST_AUTO_TEST_CASE(QuestionHasChoices)
 {
 	CMultipleChoiceQuestion question("2+2=");
-	BOOST_REQUIRE_EQUAL(question.GetChoices().GetChoiceCount(), 0u);
 
-	{
-		CGradedChoices choices = question.GetChoices();
-		choices.AddChoice("3", false);
-		question.SetChoices(choices);
-		BOOST_REQUIRE_EQUAL(question.GetChoices().GetChoiceCount(), 1u);
-
-		{
-			GradedChoice choice = question.GetChoices().GetChoice(0);
-			BOOST_REQUIRE_EQUAL(choice.text, "3");
-			BOOST_REQUIRE_EQUAL(choice.isCorrect, false);
-		}
-	}
-
-	{
-		CGradedChoices choices = question.GetChoices();
-		choices.AddChoice("4", true);
-		question.SetChoices(choices);
-		BOOST_REQUIRE_EQUAL(question.GetChoices().GetChoiceCount(), 2u);
-		BOOST_REQUIRE_THROW(question.GetChoices().GetChoice(2), out_of_range);
-	}
-
-	{
-		GradedChoice choice = question.GetChoices().GetChoice(1);
-		BOOST_REQUIRE_EQUAL(choice.text, "4");
-		BOOST_REQUIRE_EQUAL(choice.isCorrect, true);
-	}
+	BOOST_REQUIRE_NO_THROW({ CQuestionWithChoices& question2 = dynamic_cast<CQuestionWithChoices&>(question); question2; });
 }
 
 
