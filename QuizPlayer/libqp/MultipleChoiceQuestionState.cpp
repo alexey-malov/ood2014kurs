6a1d1c6e1544652d443cf345ed59c11bcc760b0d
Alexey Malov <alexey.malov@cpslabs.net>
2014-05-16 20:43:38 +0400

    Возможность получения конкретного вопроса реализована в виде шаблонного класса состояния

--- QuizPlayer/libqp/MultipleChoiceQuestionState.cpp
+++ QuizPlayer/libqp/MultipleChoiceQuestionState.cpp
@@ -9,8 +9,7 @@ namespace qp
 using namespace std;
 
 CMultipleChoiceQuestionState::CMultipleChoiceQuestionState(CConstMultipleChoiceQuestionPtr const& question)
-:CQuestionState(question)
-,m_question(question)
+:CQuestionStateWithCustomQuestion(question)
 {
 }
 
@@ -22,10 +21,11 @@ void CMultipleChoiceQuestionState::DoSubmit()
 {
 	if (m_answerIndex)
 	{
-		CGradedChoices const& choices = m_question->GetChoices();
+		auto const& question = GetQuestion();
+		auto const& choices = question->GetChoices();
 		if (choices.GetChoice(*m_answerIndex).isCorrect)
 		{
-			m_review = make_unique<CQuestionReview>(m_question->GetScore(), true);
+			m_review = make_unique<CQuestionReview>(question->GetScore(), true);
 			return;
 		}
 	}
@@ -53,11 +53,6 @@ void CMultipleChoiceQuestionState::SetUserAnswerIndex(size_t answerIndex)
 optional_size_t CMultipleChoiceQuestionState::GetUserAnswerIndex()const
 {
 	return m_answerIndex;
-}
-
-CConstMultipleChoiceQuestionPtr CMultipleChoiceQuestionState::GetQuestion()const
-{
-	return m_question;
 }
 
 }
