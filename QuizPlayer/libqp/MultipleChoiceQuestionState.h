8364a648f3647a462a44030a53ac9085c66c0088
alisa-teplouhova <flovettee@gmail.com>
2014-05-16 21:12:56 +0400

    Класс CQuestionStateWithCustomQuestion перенесен в отдельный файл

--- QuizPlayer/libqp/MultipleChoiceQuestionState.h
+++ QuizPlayer/libqp/MultipleChoiceQuestionState.h
@@ -1,30 +1,11 @@
 #pragma once
-#include "QuestionState.h"
 #include "MultipleChoiceQuestion_fwd.h"
 #include "QuestionReview_fwd.h"
+#include "QuestionStateWithCustomQuestion.h"
 #include "types.h"
 
 namespace qp
 {
-
-template <typename QuestionType>
-class CQuestionStateWithCustomQuestion : public CQuestionState
-{
-public:
-	typedef std::shared_ptr<QuestionType const> ConstQuestionTypePtr;
-	CQuestionStateWithCustomQuestion(ConstQuestionTypePtr const& question)
-		:CQuestionState(question)
-		, m_question(question)
-	{
-	}
-
-	ConstQuestionTypePtr GetQuestion()const
-	{
-		return m_question;
-	}
-private:
-	const ConstQuestionTypePtr m_question;
-};
 
 class CMultipleChoiceQuestionState : public CQuestionStateWithCustomQuestion<CMultipleChoiceQuestion>
 {
---
+++ QuizPlayer/libqp/QuestionStateWithCustomQuestion.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "QuestionState.h"
+
+namespace qp
+{
+
+template <typename QuestionType>
+class CQuestionStateWithCustomQuestion : public CQuestionState
+{
+public:
+	typedef std::shared_ptr<QuestionType const> ConstQuestionTypePtr;
+	CQuestionStateWithCustomQuestion(ConstQuestionTypePtr const& question)
+		:CQuestionState(question)
+		, m_question(question)
+	{
+	}
+
+	ConstQuestionTypePtr GetQuestion()const
+	{
+		return m_question;
+	}
+private:
+	const ConstQuestionTypePtr m_question;
+};
+
+}
