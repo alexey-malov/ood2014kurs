fe39b993ce7c12e3cd00a9f3e9e78bea3a9c73d1
Alexey Malov <alexey.malov@cpslabs.net>
2014-05-22 20:56:17 +0400

    Вьюшка умеет сигнализщировать о сабмит реквесте

--- QuizPlayer/libqp/libqptests/QuestionViewTests.cpp
+++ QuizPlayer/libqp/libqptests/QuestionViewTests.cpp
@@ -8,29 +8,37 @@
 
 using namespace qp;
 using namespace std;
+//#pragma warning (push)
 
 BOOST_AUTO_TEST_SUITE(QuestionViewTests)
+
+struct TestQuestionView : public CQuestionView
+{
+	TestQuestionView(const CQuestionStatePtr & questionState, std::ostream & outputStream, std::istream & inputStream)
+	:CQuestionView(questionState, outputStream, inputStream)
+	, detailsWereShown(false)
+	{}
+
+	void ShowDetails() override
+	{
+		detailsWereShown = true;
+	}
+
+	void ProcessString(std::string const& inputString) override
+	{
+		inputStrings.push_back(inputString);
+		CQuestionView::ProcessString(inputString);
+	}
+
+	bool detailsWereShown;
+	vector<string> inputStrings;
+};
 
 BOOST_AUTO_TEST_CASE(QuestionViewShowsDescriptionAndDetails)
 {
 	string description = "Description";
 	auto question = make_shared<CQuestionForTest>(description, 10);
 	auto state = make_shared<CQuestionStateForTesting>(question);
-	
-	struct TestQuestionView : public CQuestionView
-	{
-		TestQuestionView(const CQuestionStatePtr & questionState, std::ostream & outputStream, std::istream & inputStream)
-		:CQuestionView(questionState, outputStream, inputStream)
-		,detailsWereShown(false)
-		{}
-
-		void ShowDetails() override
-		{
-			detailsWereShown = true;
-		}
-
-		bool detailsWereShown;
-	};
 
 	ostringstream ostrm;
 	istringstream istrm;
@@ -38,8 +46,36 @@ BOOST_AUTO_TEST_CASE(QuestionViewShowsDescriptionAndDetails)
 	BOOST_REQUIRE_NO_THROW(view.Show());
 	BOOST_CHECK_EQUAL(ostrm.str(), description + "\n");
 	BOOST_CHECK(view.detailsWereShown);
-	
+}
+
+BOOST_AUTO_TEST_CASE(HandleUseInput)
+{
+	auto question = make_shared<CQuestionForTest>("descr", 10);
+	auto state = make_shared<CQuestionStateForTesting>(question);
+	ostringstream ostrm;
+	istringstream istrm("DoSomething\n");
+	TestQuestionView view(state, ostrm, istrm);
+	BOOST_REQUIRE_NO_THROW(view.HandleUserInput());
+	BOOST_REQUIRE_EQUAL(view.inputStrings.size(), 1u);
+	BOOST_REQUIRE_EQUAL(view.inputStrings[0], "DoSomething");
+}
+
+BOOST_AUTO_TEST_CASE(SubmitRequest)
+{
+	auto question = make_shared<CQuestionForTest>("descr", 10);
+	auto state = make_shared<CQuestionStateForTesting>(question);
+	ostringstream ostrm;
+	istringstream istrm("submit\n");
+	TestQuestionView view(state, ostrm, istrm);
+	bool submitRequested = false;
+	view.SubmitRequested().connect([&submitRequested](){
+		submitRequested = true;
+	});
+	BOOST_REQUIRE_NO_THROW(view.HandleUserInput());
+	BOOST_CHECK(submitRequested);
 }
 
 
 BOOST_AUTO_TEST_SUITE_END()
+
+//#pragma warning (pop)
--- QuizPlayer/libqp/stdafx.h
+++ QuizPlayer/libqp/stdafx.h
@@ -8,6 +8,7 @@
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#pragma warning (disable: 4996)
 
 #include <string>
 #include <memory>
@@ -19,6 +20,10 @@
 #include <boost/noncopyable.hpp>
 #include <boost/format.hpp>
 #include <boost/numeric/conversion/cast.hpp>
+#pragma warning (push)
+#pragma warning (disable:4512 4913 4100 4702)
+#include <boost/signals2.hpp>
+#pragma warning (pop)
 #include <loki/Visitor.h> // Unzip loki-0.1.7.zip to SDK and setup include files of Visual studio to {sdk}/{lokidir}/include
 
 
