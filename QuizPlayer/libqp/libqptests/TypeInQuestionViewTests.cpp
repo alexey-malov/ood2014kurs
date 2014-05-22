39a5d72f525a4658392ec265ad796ed381bf28b9
Sergey <svalleks@gmail.com>
2014-05-22 19:27:55 +0400

    Добавлен файл тестов TypeInQuestionViewTests.cpp

---
+++ QuizPlayer/libqp/libqptests/TypeInQuestionViewTests.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+
+#include "TypeInQuestion.h"
+#include "TypeInQuestionView.h"
+#include "TypeInQuestionState.h"
+
+using namespace qp;
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(TypeInQuestionViewTests)
+
+BOOST_AUTO_TEST_SUITE_END()
